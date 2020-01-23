bc6af00424ac610a6d72d2044aca9d834edbfb21
Bobaba Beebe <grow.food.everywhere@gmail.com>
2020-01-23 09:05:22 -0500

    removed optimized nearest neighbor

--- NearestNeighbor.py
+++ NearestNeighbor.py
@@ -4,7 +4,7 @@ class NearestNeighbor():
   """ Approximate the shortest journey between provided points
 
   this greedy algorithm searches for the next closest point from where it is standing\n
-  without going
+  without revisiting any points. Quick, but not always accurate.
 
     """
   def __init__(self, setOfPoints, dimensions = 2):
@@ -23,23 +23,6 @@ class NearestNeighbor():
           continue 
         else: 
           currDistance = dh.pythagoreanDistance(self.setOfPoints[route[-1]], point, self.dimension)
-          if currDistance < closestDistance: 
-            closestDistance = currDistance 
-            closestIndex = self.setOfPoints.index(point)
-      route.append(closestIndex)
-
-    return route
-  
-  def optimize_path(self):
-    route = [0]
-    while len(route) < len(self.setOfPoints):
-      closestDistance = 9999999999
-      closestIndex = 0
-      for point in self.setOfPoints:
-        if self.setOfPoints.index(point) in route: 
-          continue 
-        else: 
-          currDistance = dh.pythagoreanDistance(self.setOfPoints[route[-1]], point)
           if currDistance < closestDistance: 
             closestDistance = currDistance 
             closestIndex = self.setOfPoints.index(point)
